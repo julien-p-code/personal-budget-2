379484ffa7f80c1838131593b8f82758b4f74e1d
julien-p-code <229524075+julien-p-code@users.noreply.github.com>
2026-04-24 16:22:36 +0200

    Add transactions table to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,11 @@ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
 envelope_amount NUMERIC (12, 2) NOT NULL CHECK (envelope_amount >= 0)
 );
+
+CREATE TABLE transactions (
+id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+payment_amount NUMERIC (12,2) NOT NULL CHECK (payment_amount > 0),
+payment_recipient VARCHAR (100) NOT NULL,
+payment_date DATE NOT NULL,
+id_envelope INT NOT NULL REFERENCES envelopes (id)
+);
